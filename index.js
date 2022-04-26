746f8f67d2013c2284d74a60129b9d281e11c26e
Fábio da Rosa <fabdarosa@gmail.com>
2022-04-26 18:33:20 -0300

    2

--- index.js
+++ index.js
@@ -16,17 +16,6 @@ const client = new tmi.client(opts);
 client.connect();
 
 const httpServer = createServer((req, res) => {
-  if (req.url === "/front-scoket.js") {
-    const content = readFileSync("front-scoket.js");
-    const length = Buffer.byteLength(content);
-
-    res.writeHead(200, {
-      "Content-Type": "text/javascript",
-      "Content-Length": length
-    });
-    res.end(content);
-  }
-
   if (req.url !== "/") {
     res.writeHead(404);
     res.end("Not found");
